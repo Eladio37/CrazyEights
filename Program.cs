d9a43e40851e8a1b50fb59b1659d03311bb3d0a2
Eladio Rodriguez <eladiojuniorrodriguez@gmail.com>
2019-04-01 00:24:41 -0400

    Added the function to do a loop to take cards

--- Program.cs
+++ Program.cs
@@ -6,16 +6,13 @@ using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 
-namespace ProjectoFinal
-{
-    class Program
-    {
+namespace ProjectoFinal {
+    class Program {
         public static string currentCarta = "";
         public static string tipoCartaActual = "";
         public static int numeroCartaActual;
-        static void Main(string[] args)
-        {
-            Console.Clear();
+        static void Main (string[] args) {
+            Console.Clear ();
             //1. Crear cartas
             bool[] cartas = new bool[52];
             int currentPlayer = 1;
@@ -23,61 +20,55 @@ namespace ProjectoFinal
             bool finish = false;
             bool draw = false;
             //Crear los dos jugadores
-            List<int> Jugador1 = new List<int>();
-            List<int> Jugador2 = new List<int>();
+            List<int> Jugador1 = new List<int> ();
+            List<int> Jugador2 = new List<int> ();
 
             //2. Inicializar cartas en falso, asi de da a entender que no se han utilizado ninguna.
-            for (int i = 0; i < 52; i++)
-            {
+            for (int i = 0; i < 52; i++) {
                 cartas[i] = false;
             }
 
             //3. Repartir cartas
-            Repartir(Jugador1, cartas);
-            Repartir(Jugador2, cartas);
+            Repartir (Jugador1, cartas);
+            Repartir (Jugador2, cartas);
 
             //3.1. Pone la primera carta aleatoria en la mesa.
-            int cartasRestantes = contarCartas(cartas);
-            Random random = new Random();
-            int cartaActual = random.Next(0, 52);
-            while (cartas[cartaActual] != false)
-            {
-                cartaActual = random.Next(0, 52);
+            int cartasRestantes = contarCartas (cartas);
+            Random random = new Random ();
+            int cartaActual = random.Next (0, 52);
+            while (cartas[cartaActual] != false) {
+                cartaActual = random.Next (0, 52);
             }
 
             //Turnos de jugadores
-            Console.WriteLine("**************");
-            Console.WriteLine("PRIMERA JUGADA");
-            Console.WriteLine("**************");
-            while (finish != true || draw != true)
-            {
-                
+            Console.WriteLine ("**************");
+            Console.WriteLine ("PRIMERA JUGADA");
+            Console.WriteLine ("**************");
+            while (finish != true || draw != true) {
+
                 //4. Este codigo muestra la carta actual que esta en la mesa.
-                Console.WriteLine($"\n La carta actual en la mesa es: {TextoCarta(cartaActual)}. Restan [{cartasRestantes}] en el maso.");
-                numeroCartaActual = ObtenerNumeroCarta(cartaActual);
-                tipoCartaActual = ObtenerTipoCarta(cartaActual);
+                Console.WriteLine ($"\n La carta actual en la mesa es: {TextoCarta(cartaActual)}. Restan [{cartasRestantes}] en el maso.");
+                numeroCartaActual = ObtenerNumeroCarta (cartaActual);
+                tipoCartaActual = ObtenerTipoCarta (cartaActual);
                 //5. Determina cual jugador va actualmente, mediante la variable currentPlayer
-                Console.WriteLine($"\n Le toca al Jugador {currentPlayer}. Las cartas del Jugador {currentPlayer} son: ");
+                Console.WriteLine ($"\n Le toca al Jugador {currentPlayer}. Las cartas del Jugador {currentPlayer} son: ");
                 if (currentPlayer == 1)
-                    Imprimir(Jugador1); // Si es jugador 1, imprime
+                    Imprimir (Jugador1); // Si es jugador 1, imprime
                 else if (currentPlayer == 2)
-                    Imprimir(Jugador2); // Si es jugador 2, imprime
+                    Imprimir (Jugador2); // Si es jugador 2, imprime
 
-
-                Console.Write($"Jugador {currentPlayer}, elige una carta de tu mano para jugar (-1 para tomar del maso): ");
+                Console.Write ($"Jugador {currentPlayer}, elige una carta de tu mano para jugar (-1 para tomar del maso): ");
                 //Aqui se captura la carta que va a jugar, o si va a ceder turno (-1)
                 // Se guarda en la variable cardtoPlay, para saber el index de la carta en la mano de cada jugador.
                 // Para asi poder eliminarla de su mano mas tarde.
-                cardtoPlay = int.Parse(Console.ReadLine()) - 1; // El -1 es porque los indices son en base a 0, y esto empieza en 1.
-                if (cardtoPlay < 0)
-                {
+                cardtoPlay = int.Parse (Console.ReadLine ()) - 1; // El -1 es porque los indices son en base a 0, y esto empieza en 1.
+                if (cardtoPlay < 0) {
                     // Console.WriteLine("Has cedido tu turno.");
                     if (currentPlayer == 1) {
-                        TomarCarta(Jugador1, cartas);
+                        TomarCarta (Jugador1, cartas);
                         cartasRestantes--;
-                    }
-                    else if (currentPlayer == 2) {
-                        TomarCarta(Jugador1, cartas);
+                    } else if (currentPlayer == 2) {
+                        TomarCarta (Jugador1, cartas);
                         cartasRestantes--;
                     }
                 }
@@ -85,114 +76,87 @@ namespace ProjectoFinal
                 else if (currentPlayer == 1)
                     //Este if es para ver si el tipo, o el numero coincide.
                     // Me falta agregar que el 8 siempre se pueda.
-                    if (ObtenerNumeroCarta(Jugador1[cardtoPlay]) == numeroCartaActual || ObtenerTipoCarta(Jugador1[cardtoPlay]) == tipoCartaActual)
-                    {
+                    if (ObtenerNumeroCarta (Jugador1[cardtoPlay]) == numeroCartaActual || ObtenerTipoCarta (Jugador1[cardtoPlay]) == tipoCartaActual) {
                         // Si todo coincide, se le quita la carta de la mano para simular que la jugo.
                         cartaActual = Jugador1[cardtoPlay];
-                        Jugador1.RemoveAt(cardtoPlay);
+                        Jugador1.RemoveAt (cardtoPlay);
                     }
-                    else
-                        //No deberia ceder turno, sino coger del maso.
-                        Console.WriteLine("No puedes jugar esta carta, has perdido tu turno!");
+                else
+                    //No deberia ceder turno, sino coger del maso.
+                    Console.WriteLine ("No puedes jugar esta carta, has perdido tu turno!");
                 //Falta por agregar.
                 else if (currentPlayer == 2)
                     // Lo mismo de ahorita
-                    if (ObtenerNumeroCarta(Jugador2[cardtoPlay]) == numeroCartaActual || ObtenerTipoCarta(Jugador2[cardtoPlay]) == tipoCartaActual)
-                    {
+                    if (ObtenerNumeroCarta (Jugador2[cardtoPlay]) == numeroCartaActual || ObtenerTipoCarta (Jugador2[cardtoPlay]) == tipoCartaActual) {
                         cartaActual = Jugador2[cardtoPlay];
-                        Jugador2.RemoveAt(cardtoPlay);
+                        Jugador2.RemoveAt (cardtoPlay);
                     }
-                    else
-                        //No deberia ceder turno, sino coger del maso.
-                        Console.WriteLine("No puedes jugar esta carta, has perdido tu turno!");
+                else
+                    //No deberia ceder turno, sino coger del maso.
+                    Console.WriteLine ("No puedes jugar esta carta, has perdido tu turno!");
 
                 //Falta por agregar.
 
                 //Muestra la mano del jugador luego de haber jugado su carta
-                Console.WriteLine($"Jugador {currentPlayer}, tus cartas ahora son: ");
-                if (currentPlayer == 1)
-                {
-                    Imprimir(Jugador1);
+                Console.WriteLine ($"Jugador {currentPlayer}, tus cartas ahora son: ");
+                if (currentPlayer == 1) {
+                    Imprimir (Jugador1);
                     //Aqui se cambia de jugador
                     currentPlayer = 2;
-                }
-                else if (currentPlayer == 2)
-                {
-                    Imprimir(Jugador2);
+                } else if (currentPlayer == 2) {
+                    Imprimir (Jugador2);
                     //Aqui se cambia de jugador
                     currentPlayer = 1;
                 }
 
-
             }
 
         }
-        public static void Repartir(List<int> Jugador, bool[] cartas)
-        {
-            Random random = new Random();
+        public static void Repartir (List<int> Jugador, bool[] cartas) {
+            Random random = new Random ();
             int newValue;
-            for (int b = 0; b < 5; b++)
-            {
-                newValue = random.Next(0, 52);
-                while (cartas[newValue] != false)
-                {
-                    newValue = random.Next(0, 52);
+            for (int b = 0; b < 5; b++) {
+                newValue = random.Next (0, 52);
+                while (cartas[newValue] != false) {
+                    newValue = random.Next (0, 52);
                 }
-                Jugador.Add(newValue);
+                Jugador.Add (newValue);
                 cartas[newValue] = true;
 
             }
         }
-        public static void TomarCarta(List<int> Jugador, bool[] cartas)
-        {
-            Random random = new Random();
+        public static void TomarCarta (List<int> Jugador, bool[] cartas) {
+            Random random = new Random ();
             int newValue;
-            newValue = random.Next(0, 52);
-            while (cartas[newValue] != false)
-            {
-                newValue = random.Next(0, 52);
+            newValue = random.Next (0, 52);
+            while (cartas[newValue] != false) {
+                newValue = random.Next (0, 52);
             }
-            Jugador.Add(newValue);
+            Jugador.Add (newValue);
             cartas[newValue] = true;
 
         }
-        public static string ObtenerTipoCarta(int carta)
-        {
-            if (carta < 13)
-            {
+        public static string ObtenerTipoCarta (int carta) {
+            if (carta < 13) {
                 return "Corazones";
-            }
-            else if (carta < 26)
-            {
+            } else if (carta < 26) {
                 return "Diamantes";
-            }
-            else if (carta < 39)
-            {
+            } else if (carta < 39) {
                 return "Trebol";
-            }
-            else if (carta < 52)
-            {
+            } else if (carta < 52) {
                 return "Picas";
-            }
-            else
+            } else
                 return null;
         }
-        public static int ObtenerNumeroCarta(int carta)
-        {
+        public static int ObtenerNumeroCarta (int carta) {
             int modulo;
             modulo = carta % 13;
-            if (modulo == 0)
-            {
+            if (modulo == 0) {
                 return 1;
-            }
-            else if (modulo < 10)
-            {
+            } else if (modulo < 10) {
                 return modulo + 1;
-            }
-            else
-            {
-                switch (modulo)
-                {
+            } else {
+                switch (modulo) {
                     case 10:
                         return 11;
                     case 11:
@@ -203,60 +167,41 @@ namespace ProjectoFinal
             }
             return 0;
         }
-        public static void Imprimir(List<int> Jugador)
-        {
+        public static void Imprimir (List<int> Jugador) {
             int numero = 1;
             string carta = "";
 
-            foreach (var Imprimir in Jugador)
-            {
-                carta = TextoCarta(Imprimir);
-                Console.WriteLine(numero + ". " + carta);
+            foreach (var Imprimir in Jugador) {
+                carta = TextoCarta (Imprimir);
+                Console.WriteLine (numero + ". " + carta);
                 numero++;
             }
         }
-        public static string TextoCarta(int valor)
-        {
+        public static string TextoCarta (int valor) {
             int modulo;
             string carta = "", tipo = "";
             modulo = valor % 13;
 
-            if (valor < 13)
-            {
+            if (valor < 13) {
                 tipo = "de Corazones";
-            }
-            else if (valor < 26)
-            {
+            } else if (valor < 26) {
                 tipo = "de Diamantes";
-            }
-            else if (valor < 39)
-            {
+            } else if (valor < 39) {
                 tipo = "de Treboles";
-            }
-            else
-            {
+            } else {
                 tipo = "de Picas";
             }
 
-            if (modulo == 0)
-            {
+            if (modulo == 0) {
                 carta = "A  " + tipo;
-            }
-            else if (modulo < 10)
-            {
-                if (modulo == 9)
-                {
-                    carta = (modulo + 1).ToString() + " " + tipo;
+            } else if (modulo < 10) {
+                if (modulo == 9) {
+                    carta = (modulo + 1).ToString () + " " + tipo;
+                } else {
+                    carta = (modulo + 1).ToString () + "  " + tipo;
                 }
-                else
-                {
-                    carta = (modulo + 1).ToString() + "  " + tipo;
-                }
-            }
-            else
-            {
-                switch (modulo)
-                {
+            } else {
+                switch (modulo) {
                     case 10:
                         carta = "J  " + tipo;
                         break;
@@ -285,13 +230,10 @@ namespace ProjectoFinal
         //     tipoCartaActual = ObtenerTipoCarta(cartaActual);
         //     Console.WriteLine($"\n La carta actual en la mesa es: {currentCarta}. Restan [{contador}] en el maso.");
         // }
-        public static int contarCartas(bool[] cartas)
-        {
+        public static int contarCartas (bool[] cartas) {
             int contador = 0;
-            for (int i = 0; i < 52; i++)
-            {
-                if (cartas[i] == false)
-                {
+            for (int i = 0; i < 52; i++) {
+                if (cartas[i] == false) {
                     contador++;
                 }
             }
